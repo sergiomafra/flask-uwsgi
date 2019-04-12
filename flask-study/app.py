769909842d558eb124ea453a1432fee978e0856d
Sérgio Mafra <sergio@mafra.io>
2019-04-12 00:37:16 -0300

    Flask Study - Dynamic routing

--- flask-study/app.py
+++ flask-study/app.py
@@ -2,13 +2,20 @@ from flask import Flask
 
 app = Flask(__name__)
 
+## Hello World
 @app.route('/')
 def index():
     return '<h1>Hello Puppy!</h1>'
 
+## Creating another unique route
 @app.route('/information')
 def info():
     return '<h1>Puppies are cute!</h1>'
+
+## Example of dynamic route
+@app.route('/puppy/<name>')
+def puppy(name):
+    return f'<h1>This is a page for {name.upper()}</h1>'
 
 if __name__ == '__main__':
     app.run()
